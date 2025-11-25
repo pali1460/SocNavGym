2756bd35726a9e1311785043f02f1a8680c3e6a1
Daniel Zhu <daniel.zhu@colorado.edu>
2025-11-25 15:04:21 -0700

    test

---
+++ circle_proxemic_reward.py
@@ -0,0 +1,167 @@
+import socnavgym
+from socnavgym.envs.socnavenv_v1 import SocNavEnv_v1, EntityObs
+from socnavgym.envs.utils.utils import point_to_segment_dist
+import numpy as np
+from socnavgym.envs.rewards.reward_api import RewardAPI
+from enum import Enum
+import matplotlib.patches as patches
+
+
+class Reward(RewardAPI):
+    def __init__(self, env: SocNavEnv_v1) -> None:
+        super().__init__(env)
+        self.reach_reward = 1.0
+        self.out_of_map_reward = -1.0 
+        self.max_steps_reward = -1.0 
+        self.alive_reward = -0.00001 
+        self.collision_reward = -1.0
+        self.distance_reward_scaler = 5.0
+        self.discomfort_distance = 0.6
+        self.discomfort_penalty_factor = 0.5
+        self.prev_distance = None
+
+    # compute the minimum distance between the robot and all humans/interactions over the next timestep
+    def compute_dmin(self, action):
+        dmin = float('inf')
+
+        all_humans = []
+        for human in self.env.static_humans + self.env.dynamic_humans : all_humans.append(human)
+
+        for i in self.env.static_interactions + self.env.moving_interactions:
+            for h in i.humans: all_humans.append(h)
+        
+        for i in self.env.h_l_interactions: all_humans.append(i.human)
+
+        for human in all_humans:
+            px = human.x - self.env.robot.x
+            py = human.y - self.env.robot.y
+
+            vx = human.speed*np.cos(human.orientation) - action[0] * np.cos(action[2]*self.env.TIMESTEP + self.env.robot.orientation) - action[1] * np.cos(action[2]*self.env.TIMESTEP + self.env.robot.orientation + np.pi/2)
+            vy = human.speed*np.sin(human.orientation) - action[0] * np.sin(action[2]*self.env.TIMESTEP + self.env.robot.orientation) - action[1] * np.sin(action[2]*self.env.TIMESTEP + self.env.robot.orientation + np.pi/2)
+
+            ex = px + vx * self.env.TIMESTEP
+            ey = py + vy * self.env.TIMESTEP
+
+            closest_dist = point_to_segment_dist(px, py, ex, ey, 0, 0) - self.env.HUMAN_DIAMETER/2 - self.env.ROBOT_RADIUS
+
+            if closest_dist < dmin:
+                dmin = closest_dist
+
+        for human in self.env.static_humans + self.env.dynamic_humans:
+            px = human.x - self.env.robot.x
+            py = human.y - self.env.robot.y
+
+            vx = human.speed*np.cos(human.orientation) - action[0] * np.cos(action[2]*self.env.TIMESTEP + self.env.robot.orientation) - action[1] * np.cos(action[2]*self.env.TIMESTEP + self.env.robot.orientation + np.pi/2)
+            vy = human.speed*np.sin(human.orientation) - action[0] * np.sin(action[2]*self.env.TIMESTEP + self.env.robot.orientation) - action[1] * np.sin(action[2]*self.env.TIMESTEP + self.env.robot.orientation + np.pi/2)
+
+            ex = px + vx * self.env.TIMESTEP
+            ey = py + vy * self.env.TIMESTEP
+
+            closest_dist = point_to_segment_dist(px, py, ex, ey, 0, 0) - self.env.HUMAN_DIAMETER/2 - self.env.ROBOT_RADIUS
+
+            if closest_dist < dmin:
+                dmin = closest_dist
+
+        for interaction in (self.env.moving_interactions + self.env.static_interactions + self.env.h_l_interactions):
+            px = interaction.x - self.env.robot.x
+            py = interaction.y - self.env.robot.y
+
+            speed = 0
+            if interaction.name == "human-human-interaction":
+                for h in interaction.humans:
+                    speed += h.speed
+                speed /= len(interaction.humans)
+
+            vx = speed*np.cos(human.orientation) - action[0] * np.cos(action[2]*self.env.TIMESTEP + self.env.robot.orientation) - action[1] * np.cos(action[2]*self.env.TIMESTEP + self.env.robot.orientation + np.pi/2)
+            vy = speed*np.sin(human.orientation) - action[0] * np.sin(action[2]*self.env.TIMESTEP + self.env.robot.orientation) - action[1] * np.sin(action[2]*self.env.TIMESTEP + self.env.robot.orientation + np.pi/2)
+
+            ex = px + vx * self.env.TIMESTEP
+            ey = py + vy * self.env.TIMESTEP
+
+            closest_dist = point_to_segment_dist(px, py, ex, ey, 0, 0) - self.env.HUMAN_DIAMETER/2 - self.env.ROBOT_RADIUS
+
+            if closest_dist < dmin:
+                dmin = closest_dist
+        
+        return dmin
+        
+
+    def compute_reward(self, action, prev_obs: EntityObs, curr_obs: EntityObs):
+        if self.check_out_of_map(): return self.out_of_map_reward
+        elif self.check_reached_goal(): return self.reach_reward
+        elif self.check_collision(): return self.collision_reward
+        elif self.check_timeout(): return self.max_steps_reward
+        else:
+            dsrnn_reward = 0.0
+            dmin = self.compute_dmin(action)
+            if dmin < self.discomfort_distance:
+                dsrnn_reward = (dmin - self.discomfort_distance) * self.discomfort_penalty_factor * self.env.TIMESTEP
+
+            distance_to_goal = np.sqrt((self.env.robot.goal_x - self.env.robot.x)**2 + (self.env.robot.goal_y - self.env.robot.y)**2)
+            distance_reward = 0.0
+            if self.prev_distance is not None:
+                distance_reward = -(distance_to_goal-self.prev_distance) * self.distance_reward_scaler
+            
+            self.prev_distance = distance_to_goal
+
+            self.info["DISCOMFORT_SNGNN"] = 0.0
+            self.info["DISCOMFORT_DSRNN"] = dsrnn_reward
+            self.info["distance_reward"] = distance_reward
+            self.info["alive_reward"] = self.alive_reward
+            self.info["proxemics_reward"] = self.compute_proxemics()
+
+            return dsrnn_reward + distance_reward + self.alive_reward + self.info["proxemics_reward"]
+    
+    def compute_proxemics(self):
+        reward = 0
+        robot_pos = np.array(self.env.robot.position)
+
+        for human in self.env.static_humans + self.env.dynamic_humans:
+            dist = np.linalg.norm(robot_pos - np.array(human.position))
+            proxemic_zone = self.get_proxemic_zone(dist)
+
+            # these are just arbitarily chosen values for now
+            match proxemic_zone:
+                case self.Proxemic_Zone.INTIMETE:
+                    reward -= 1.0
+                case self.Proxemic_Zone.PERSONAL:
+                    reward -= 0.5
+                case self.Proxemic_Zone.SOCIAL:
+                    reward -= 0.1
+                case self.Proxemic_Zone.PUBLIC:
+                    reward += 0.0
+
+        return reward
+    
+    class Proxemic_Zone(Enum):
+        INTIMETE = 0
+        PERSONAL = 1
+        SOCIAL = 2
+        PUBLIC = 3
+    
+    # Circle proxemic model, so just check distance to determine zone
+    # Values based on Hall's paper on proxemics, in meters
+    # May differ for robot interaction & cultures & scenarios
+    def get_proxemic_zone(self, distance):
+        if distance < 0:
+            raise ValueError("Distance cannot be negative")
+        
+        if distance < 0.46:
+            return self.Proxemic_Zone.INTIMETE
+        elif distance < 1.2:
+            return self.Proxemic_Zone.PERSONAL
+        elif distance < 3.7:
+            return self.Proxemic_Zone.SOCIAL
+        else:
+            return self.Proxemic_Zone.PUBLIC
+    
+    # idk how to render rn
+    def get_pathches(self):
+        hx = self.env.robot.x
+        hy = self.env.robot.y
+
+        return [
+            patches.Circle((hx, hy), 0.45, color='red', alpha=0.25),
+            patches.Circle((hx, hy), 1.2, color='yellow', alpha=0.25),
+            patches.Circle((hx, hy), 3.7, color='green', alpha=0.25)
+        ]
